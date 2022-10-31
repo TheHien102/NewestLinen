b095c4bb7b0764f10cdae74a4e5654627cf8c452
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-10-31 13:03:47 +0700

    Initial commit

---
+++ src/main/java/com/example/newestlinen/NewestLinenApplication.java
@@ -0,0 +1,13 @@
+package com.example.newestlinen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewestLinenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewestLinenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newestlinen/NewestLinenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newestlinen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewestLinenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
