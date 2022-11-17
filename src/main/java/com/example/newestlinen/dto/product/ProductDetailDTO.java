ed083335d2c4788a8abf0d40991ff76072edf824
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-11-17 20:08:24 +0700

    update category description

---
+++ src/main/java/com/example/newestlinen/dto/product/ProductDetailDTO.java
@@ -0,0 +1,28 @@
+package com.example.newestlinen.dto.product;
+
+import com.example.newestlinen.storage.model.ProductModel.Asset;
+import com.example.newestlinen.storage.model.ProductModel.Review;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductDetailDTO {
+    private Long id;
+
+    private String name;
+
+    private String mainImg;
+
+    private int discount;
+
+    private String description;
+
+    private int price;
+
+    private String categoryDescription;
+
+    private List<Asset> assets;
+
+    private List<Review> reviews;
+}
