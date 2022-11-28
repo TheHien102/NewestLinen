16eb8ec3452cac4fbf1cfcd143f5792b9a85f80c
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-11-28 13:29:15 +0700

    update change profile info

--- src/main/java/com/example/newestlinen/form/account/UpdateProfileUserForm.java
+++ src/main/java/com/example/newestlinen/form/account/UpdateProfileUserForm.java
@@ -11,14 +11,10 @@ import javax.validation.constraints.Size;
 @Data
 public class UpdateProfileUserForm {
     @Schema(name = "username")
-    @NotBlank(message = "username should not be empty")
-    @NotEmpty(message = "username should not be empty")
     @Size(min = 6, max = 18, message = "username should between {min} and {max} character")
     private String username;
 
     @Schema(name = "password")
-    @NotBlank(message = "password should not be empty")
-    @NotEmpty(message = "password should not be empty")
     @Size(min = 8, max = 16, message = "password should between {min} and {max} character")
     private String password;
 
@@ -26,8 +22,6 @@ public class UpdateProfileUserForm {
     private String oldPassword;
 
     @Schema(name = "fullName")
-    @NotBlank(message = "fullName should not be empty")
-    @NotEmpty(message = "fullName should not be empty")
     @Size(min = 1, max = 50, message = "fullName should between {min} and {max} character")
     private String fullName;
 
@@ -36,14 +30,10 @@ public class UpdateProfileUserForm {
 
     @Email(message = "invalid email address")
     @Schema(name = "email")
-    @NotBlank(message = "email should not be empty")
-    @NotEmpty(message = "email should not be empty")
     @Size(min = 8, max = 50, message = "email should between {min} and {max} character")
     private String email;
 
     @Schema(name = "phone")
-    @NotBlank(message = "phone should not be empty")
-    @NotEmpty(message = "phone should not be empty")
     @Size(min = 10, max = 11, message = "phone should between {min} and {max} character")
     private String phone;
 }
