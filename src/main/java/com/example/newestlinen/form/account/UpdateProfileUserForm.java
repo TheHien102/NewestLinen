b6e2d16b06bae772a1dc979d748eccd283be40e6
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-11-28 15:11:30 +0700

    update change profile info

--- src/main/java/com/example/newestlinen/form/account/UpdateProfileUserForm.java
+++ src/main/java/com/example/newestlinen/form/account/UpdateProfileUserForm.java
@@ -3,10 +3,7 @@ package com.example.newestlinen.form.account;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 public class UpdateProfileUserForm {
