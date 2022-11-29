d756dcb36cba9b442ed7591cc7ef2a0879344fe2
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-11-29 23:19:25 +0700

    .

--- src/main/java/com/example/newestlinen/NewestLinenApplication.java
+++ src/main/java/com/example/newestlinen/NewestLinenApplication.java
@@ -44,7 +44,7 @@ public class NewestLinenApplication {
     PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     private void createAdminUserIfNotExist() {
-        Account account = qrCodeStorageService.findAccountByUsername("admin1");
+        Account account = qrCodeStorageService.findAccountByUsername("admin");
         if (account == null) {
             List<Permission> defaultPermission = addPermission();
             Group group = initGroupDefault(defaultPermission);
