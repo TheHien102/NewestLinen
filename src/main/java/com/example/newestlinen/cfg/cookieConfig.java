4e91b7f403f33e4cf76cfca5bd59f3bf1c4a09a4
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-01 14:43:07 +0700

    fix session time out

---
+++ src/main/java/com/example/newestlinen/cfg/cookieConfig.java
@@ -0,0 +1,13 @@
+package com.example.newestlinen.cfg;
+
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class cookieConfig {
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
+    }
+}
