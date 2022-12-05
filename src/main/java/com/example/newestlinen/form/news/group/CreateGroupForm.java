0eef202c79de3cb5332282755b2b613f83ee0294
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-05 12:33:19 +0700

    create group form validation update

--- src/main/java/com/example/newestlinen/form/news/group/CreateGroupForm.java
+++ src/main/java/com/example/newestlinen/form/news/group/CreateGroupForm.java
@@ -4,6 +4,8 @@ import com.example.newestlinen.dto.permission.PermissionDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -11,16 +13,23 @@ import java.util.List;
 @Data
 @Schema
 public class CreateGroupForm {
+    @NotBlank(message = "Name cant not be null")
     @NotEmpty(message = "Name cant not be null")
     @Schema(name = "name", required = true)
     private String name;
+
+    @NotBlank(message = "Name cant not be null")
     @NotEmpty(message = "description cant not be null")
     @Schema(name = "description", required = true)
     private String description;
+
     @NotNull(message = "permissions cant not be null")
+    @Min(value = 1,message = "permissions cant not be null")
     @Schema(name = "permissions", required = true)
     private List<PermissionDto> permissions;
+
     @NotNull(message = "kind cant not be null")
+    @Min(value = 1,message = "kind cant not be null")
     @Schema(name = "kind", required = true)
     private Integer kind;
 }
