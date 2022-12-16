2d7cb3cfa65d82cea8ce7e7f4c4c8e4de5cc7c69
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-16 10:52:13 +0700

    clear print console

--- src/main/java/com/example/newestlinen/service/EmailService.java
+++ src/main/java/com/example/newestlinen/service/EmailService.java
@@ -1,8 +1,8 @@
 package com.example.newestlinen.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +13,8 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class EmailService {
 
-    private final JavaMailSender emailSender = new JavaMailSenderImpl();
+    @Autowired
+    private JavaMailSender emailSender;
 
     public void sendEmail(String email, String msg, String subject, boolean html) {
         try {
@@ -22,13 +23,14 @@ public class EmailService {
             MimeMessageHelper helper = new MimeMessageHelper(message,
                     MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                     StandardCharsets.UTF_8.name());
+            helper.setFrom("Linen A");
             helper.setTo(email);
             helper.setSubject(subject);
-            helper.setText(msg, html);
+            helper.setText(msg,html);
 
             emailSender.send(message);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
+        } catch (Exception e){
+            log.error(e.getMessage(),e);
         }
     }
 
