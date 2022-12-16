28c389ab94229af037f6df43e7c7ddaea37f6ebf
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-16 09:43:25 +0700

    fix mail

--- src/main/java/com/example/newestlinen/NewestLinenApplication.java
+++ src/main/java/com/example/newestlinen/NewestLinenApplication.java
@@ -40,11 +40,6 @@ public class NewestLinenApplication {
     @Autowired
     PermissionRepository permissionRepository;
 
-    @Bean
-    JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(NewestLinenApplication.class, args);
     }
