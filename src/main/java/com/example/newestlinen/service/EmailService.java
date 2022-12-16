1aae0c313cc0fcb91b183b75d432fc3da734e613
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-16 11:42:55 +0700

    add email template

--- src/main/java/com/example/newestlinen/service/EmailService.java
+++ src/main/java/com/example/newestlinen/service/EmailService.java
@@ -4,6 +4,7 @@ import com.example.newestlinen.storage.model.Account;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ public class EmailService {
 
     private final JavaMailSender emailSender;
     private final SpringTemplateEngine templateEngine;
+    private final Environment env;
 
     public void sendEmail(Account account, String code, String subject) {
         try {
@@ -39,7 +41,7 @@ public class EmailService {
             MimeMessageHelper helper = new MimeMessageHelper(message,
                     MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                     StandardCharsets.UTF_8.name());
-            helper.setFrom("Linen A");
+            helper.setFrom(new InternetAddress(env.getProperty("spring.mail.username"),"Linen À"));
             helper.setTo(account.getEmail());
             helper.setSubject(subject);
             helper.setText(html, true);
