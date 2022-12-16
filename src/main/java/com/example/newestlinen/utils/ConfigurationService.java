5dc02e08cc200aec3980cbf0dd39a72f567d0885
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-16 12:09:47 +0700

    add email template 's logo

--- src/main/java/com/example/newestlinen/utils/ConfigurationService.java
+++ src/main/java/com/example/newestlinen/utils/ConfigurationService.java
@@ -6,7 +6,6 @@
 package com.example.newestlinen.utils;
 
 /**
- *
  * @author cht
  */
 
@@ -21,54 +20,55 @@ import java.io.File;
 import java.io.IOException;
 
 @Slf4j
-public class ConfigurationService extends PropertiesConfiguration implements ConfigurationListener{ 
-	   private static final String PROPS_RESOURCE = "config.properties";
-	   private static ConfigurationService instance = null;
-           private ConfigurationService(){
-           
-           }
-	   public static final ConfigurationService getInstance() {
-		      if (instance == null) {
-		         instance = new ConfigurationService();
-		         instance.loadProperties();
-		      }
-		      return instance;
-		   }
-	   private void loadProperties() {
-		      try {
-		         File is = new File(PROPS_RESOURCE);
-		         loadProperties(is);
-		      }
-		      catch (IOException ioe) {
-		         log.warn("Could not load engine properties", ioe);
-		      }
-		   }
+public class ConfigurationService extends PropertiesConfiguration implements ConfigurationListener {
+    private static final String PROPS_RESOURCE = "config.properties";
+    private static ConfigurationService instance = null;
 
-	   private void loadProperties(File is)
-	      throws IOException {
-	      try {
-	         instance.setFile(is);
-	         instance.setReloadingStrategy(new FileChangedReloadingStrategy());
-	         instance.setAutoSave(true);
-	         instance.load();
-			  log.info("Loaded engine properties from " + PROPS_RESOURCE);
-	      }
-	      catch (ConfigurationException e) {
-	         throw new IOException("Failed to load properties from " + PROPS_RESOURCE);
-	      }
-	   }
-	   
-	   
-	@Override
-	public void configurationChanged() {
-		super.configurationChanged();
-		log.info("something change in config.properties file, configurationChanged()!");
-	}
-        @Override
-	public void configurationChanged(ConfigurationEvent event) {
-    	log.info("something change in config.properties file, configurationChanged(ConfigurationEvent event)!");
-	}
-	   
+    private ConfigurationService() {
+
+    }
+
+    public static ConfigurationService getInstance() {
+        if (instance == null) {
+            instance = new ConfigurationService();
+            instance.loadProperties();
+        }
+        return instance;
+    }
+
+    private void loadProperties() {
+        try {
+            File is = new File(PROPS_RESOURCE);
+            loadProperties(is);
+        } catch (IOException ioe) {
+            log.warn("Could not load engine properties", ioe);
+        }
+    }
+
+    private void loadProperties(File is)
+            throws IOException {
+        try {
+            instance.setFile(is);
+            instance.setReloadingStrategy(new FileChangedReloadingStrategy());
+            instance.setAutoSave(true);
+            instance.load();
+            log.info("Loaded engine properties from " + PROPS_RESOURCE);
+        } catch (ConfigurationException e) {
+            throw new IOException("Failed to load properties from " + PROPS_RESOURCE);
+        }
+    }
+
+
+    @Override
+    public void configurationChanged() {
+        super.configurationChanged();
+        log.info("something change in config.properties file, configurationChanged()!");
+    }
+
+    @Override
+    public void configurationChanged(ConfigurationEvent event) {
+        log.info("something change in config.properties file, configurationChanged(ConfigurationEvent event)!");
+    }
 
 
 }
