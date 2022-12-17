391b9491e1e3fb8deb3d72e45a25016835e2b460
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-17 15:55:07 +0700

    validate pass reset

--- src/main/java/com/example/newestlinen/form/account/ForgetPasswordForm.java
+++ src/main/java/com/example/newestlinen/form/account/ForgetPasswordForm.java
@@ -3,6 +3,7 @@ package com.example.newestlinen.form.account;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -17,8 +18,9 @@ public class ForgetPasswordForm {
     @Schema(name = "idHash", required = true)
     private String idHash;
 
-    @NotEmpty(message = "newPassword can not be null")
-    @Size(min = 6, message = "newPassword minimum 6 character.")
+    @NotBlank(message = "password should not be empty")
+    @NotEmpty(message = "password should not be empty")
+    @Size(min = 8, max = 16, message = "password should between {min} and {max} character")
     @Schema(name = "newPassword", required = true)
     private String newPassword;
 }
